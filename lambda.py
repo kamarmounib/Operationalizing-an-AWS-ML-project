38f69e0961b33b46181ca2d6c6051c7120747496
kamarmounib <61589670+kamarmounib@users.noreply.github.com>
2022-01-27 21:23:07 +0200

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,40 @@
+Python 3.7.7 (tags/v3.7.7:d7c567b08f, Mar 10 2020, 09:44:33) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import boto3
+#import numpy
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+print('Loading Lambda function')
+
+runtime=boto3.Session().client('sagemaker-runtime')
+endpoint_Name='pytorch-inference-2022-01-27-12-34-34-327'
+
+def lambda_handler(event, context):
+
+    #x=event['content']
+    #aa=x.encode('ascii')
+    #bs=base64.b64decode(aa)
+    print('Context:::',context)
+    print('EventType::',type(event))
+    bs=event
+    runtime=boto3.Session().client('sagemaker-runtime')
+    
+    response=runtime.invoke_endpoint(EndpointName=endpoint_Name,
+                                    ContentType="application/json",
+                                    Accept='application/json',
+                                    #Body=bytearray(x)
+                                    Body=json.dumps(bs))
+    
+    result=response['Body'].read().decode('utf-8')
+    sss=json.loads(result)
+    
+    return {
+        'statusCode': 200,
+        'headers' : { 'Content-Type' : 'text/plain', 'Access-Control-Allow-Origin' : '*' },
+        'type-result':str(type(result)),
+        'COntent-Type-In':str(context),
+        'body' : json.dumps(sss)
+        #'updated_result':str(updated_result)
+
+        }
